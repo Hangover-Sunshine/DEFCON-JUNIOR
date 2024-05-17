78f6507c076bc44586a8c2277c406cabd193dd01
SBrianZ <brian@brianzavala.com>
2024-05-17 15:33:19 -0700

    Function Line Reader

--- scripts/menus/menu_cutscene.gd
+++ scripts/menus/menu_cutscene.gd
@@ -1,8 +1,15 @@
 extends Control
 
 @onready var text = $Text_Label
+@onready var continue_text = $Continue_Label
 
+@onready var TheVoid = $TheVoid
+@onready var ap_mouth = $TheVoid/AP_Mouth
+@onready var ap_upper_face = $TheVoid/AP_UpperFace
+
+#Manipulate this guy to run specific cutscene
 var chapter = 1
+
 var line = 0
 var length = 0
 var cur_script = ["INSERT TEXT HERE"]
@@ -11,16 +18,20 @@ var script2 = ["Hola", "Coma Estas", "Bien", "Choi"]
 
 func _ready():
 	assign_script()
-	print(script1.size())
 
 func assign_script():
 	if chapter == 1:
 		cur_script = script1
+	elif chapter == 2:
+		cur_script = script2
 	text.text = cur_script[line]
 		
 func _input(event):
-	if event.is_pressed():
+	if event.is_pressed() and continue_text.visible == true:
 		line += 1
+		if line < cur_script.size():
+			ap_mouth.play("Speak")
+			continue_text.visible = false
 	next_line()
 
 func next_line():
@@ -28,3 +39,9 @@ func next_line():
 		text.text = cur_script[line]
 	elif line >= cur_script.size():
 		text.text = "SCENE END"
+		continue_text.visible = false
+		
+func _on_ap_mouth_animation_finished(anim_name):
+	if anim_name == "Speak":
+		ap_mouth.play("Neutral")
+		continue_text.visible = true
