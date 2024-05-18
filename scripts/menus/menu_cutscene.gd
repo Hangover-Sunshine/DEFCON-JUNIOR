e15b7d9fdd7da663582bd5cd2f56a920a49ac19e
SBrianZ <brian@brianzavala.com>
2024-05-17 20:04:06 -0700

    Functional Cutscene Manager

--- scripts/menus/menu_cutscene.gd
+++ scripts/menus/menu_cutscene.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@onready var post_nuke = $PostNuke
+@onready var levels_label = $PostNuke/Levels_Label
+@onready var cutscene_art = $PostNuke/Cutscene_Art
+
+@onready var ap_text = $AP_Text
+
 @onready var speech = $Speech_Labels
 @onready var text = $Speech_Labels/Text_Label
 @onready var continue_text = $Speech_Labels/Continue_Label
@@ -15,8 +21,9 @@ extends Control
 @onready var ap_cutscene = $AP_Cutscene
 
 
-# Manipulate this guy to run specific cutscene
-var chapter = 1
+# Manipulate this guy to run specific cutscene and to make scene not flash player
+var chapter = 2
+var no_flash = false
 
 # 0 - Splash Art, 1 - Conversation
 var section = 0
@@ -45,12 +52,28 @@ var script6 = ["Sixth chapter - hello", "Sixth chapter - plz stop"]
 var murder6 = "F."
 
 func _ready():
-	spawn_void()
+	start_cutscene()
 
+func start_cutscene():
+	var section = 0
+	var has_question = false
+	var give_murder = false
+	var give_mercy = false
+	var post_speech = false
+	var line = 0
+	cutscene_art.frame = chapter - 1
+	levels_label.text = str(chapter," / 7")
+	if no_flash == false:
+		ap_cutscene.play("Post-Nuke-Flash")
+	else:
+		ap_cutscene.play("Post-Nuke")
+
+# Shows NPC art
 func spawn_void():
 	ap_cutscene.play("Spawn")
 	ap_left_eye.play("Closed")
 
+# Uses end of animation to keep track of where player is in cutscene
 func _on_ap_cutscene_animation_finished(anim_name):
 	if anim_name == "Spawn" and section == 0:
 		assign_script()
@@ -63,6 +86,8 @@ func _on_ap_cutscene_animation_finished(anim_name):
 			print("Murder time!")
 		elif give_mercy == true:
 			print("Mercy time!")
+	elif anim_name == "Post-Nuke" or anim_name == "Post-Nuke-Flash":
+		spawn_void()
 
 # Check to see which chapter the game is in and load the script
 func assign_script():
@@ -86,6 +111,7 @@ func assign_script():
 		cur_murder = murder6
 	text.text = cur_script[line]
 
+# Allows player to shift through text when applicable
 func _input(event):
 	if section == 1:
 		if event.is_pressed() and continue_text.visible == true and post_speech == false:
@@ -96,24 +122,26 @@ func _input(event):
 				next_line()
 			elif line >= cur_script.size() and has_question == true:
 				continue_text.visible = false
-				speech.visible = false
-				question.visible = true
+				ap_text.play("SpeechToQuestion")
 		elif event.is_pressed() and continue_text.visible == true and post_speech == true:
 			ap_cutscene.play("Despawn")
 			speech.visible = false
 			continue_text.visible = false
 
+# Updates line and checks to notifies input function if a question is coming up
 func next_line():
 	if line < cur_script.size():
 		text.text = cur_script[line]
 		if line + 1 == cur_script.size():
 			has_question = true
 
+# Prevents player from skipping quickly through text and plays talking animation
 func _on_ap_mouth_animation_finished(anim_name):
 	if anim_name == "Speak":
 		ap_mouth.play("Neutral")
 		continue_text.visible = true
 
+# Makes TheVoid sad and sets up trigger to next level
 func _on_continue_button_pressed():
 	question.visible = false
 	give_murder = true
@@ -123,6 +151,7 @@ func _on_continue_button_pressed():
 	ap_left_eye.play("Closed")
 	speech.visible = true
 
+# Makes TheVoid greatful and sets up trigger to send player back to hub_menu
 func _on_quit_button_pressed():
 	question.visible = false
 	give_mercy = true
