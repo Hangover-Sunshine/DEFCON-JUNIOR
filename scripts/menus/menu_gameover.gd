6a9b98c4aadffdd40a6861f0570229479e715737
unknown <michaelgardone@gmail.com>
2024-05-19 01:17:54 -0600

    fixing game over bug

--- scripts/menus/menu_gameover.gd
+++ scripts/menus/menu_gameover.gd
@@ -2,6 +2,13 @@ extends Control
 
 func _ready():
 	get_tree().paused = false
+	GlobalSignals.connect("scene_loaded", _scene_loaded)
+##
+
+func _scene_loaded(scene_name):
+	if scene_name != name:
+		self.queue_free()
+	##
 ##
 
 func _on_cont_button_pressed():
